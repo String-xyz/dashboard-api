096bcffac3d32253f6967fdb331060883b04b51f
Sean <sean@string.xyz>
2023-02-03 16:19:27 -0700

    cookie

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -14,8 +14,7 @@ import (
 
 func BearerAuth() echo.MiddlewareFunc {
 	config := echoMiddleware.JWTConfig{
-		TokenLookup: "header:Authorization",
-		// TokenLookup: "header:Authorization,cookie:StringJWT", // We are not using a cookie for the JWT in this API
+		TokenLookup: "header:Authorization,cookie:StringJWT",
 		ParseTokenFunc: func(auth string, c echo.Context) (interface{}, error) {
 			var claims = &service.JWTClaims{}
 			t, err := jwt.ParseWithClaims(auth, claims, func(t *jwt.Token) (interface{}, error) {
