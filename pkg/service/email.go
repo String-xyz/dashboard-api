b6ace302f33d4f3f7a07da3d2b57dd46d2cb7174
Wilfredo Alcala <alcalawilfre@gmail.com>
2023-02-10 19:00:43 -0400

    return an error when email sending fails (#14)
    * return an error when email sending fails
    
    * Update pkg/service/email.go
    
    Co-authored-by: Auroter <7332587+Auroter@users.noreply.github.com>
    
    * fix import
    
    ---------
    
    Co-authored-by: Auroter <7332587+Auroter@users.noreply.github.com>

--- pkg/service/email.go
+++ pkg/service/email.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/String-xyz/go-lib/common"
@@ -18,7 +20,13 @@ func SendEmail(from string, to string, fromAddress string, toAddress string, sub
 	// TODO: Parse body to ensure links are valid
 	message := mail.NewSingleEmail(f, subject, t, textContent, body)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
-	_, err := client.Send(message)
+	res, err := client.Send(message)
+
+	if res.StatusCode >= 400 {
+		fmt.Println(">>> email failed to send: ", res.Body)
+		return common.StringError(errors.New(res.Body))
+	}
+
 	if err != nil {
 		return common.StringError(err)
 	}
