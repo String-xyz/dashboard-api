0d9e4c008cbe74b46c377eaa92155da67a646be2
saito-sv <7920256+saito-sv@users.noreply.github.com>
2023-06-23 10:09:07 -0700

    Update pkg/model/entity.go
    Co-authored-by: Frostbourne <85953565+frostbournesb@users.noreply.github.com>

--- pkg/model/entity.go
+++ pkg/model/entity.go
@@ -95,7 +95,7 @@ type Contract struct {
 	Address        string         `json:"address" db:"address"`
 	Functions      pq.StringArray `json:"functions" db:"functions" swaggertype:"array,string"`
 	NetworkId      string         `json:"networkId" db:"network_id"`
-	PlatformIDs    pq.StringArray `json:"platformId" db:"platform_id"`
+	PlatformIds    pq.StringArray `json:"platformIds" db:"platform_ids" swaggertype:"array,string"
 	OrganizationId string         `json:"organizationId" db:"organization_id"`
 }
 
